afffab0ad1888665e646f503f9695b02ba21cd91
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-07-28 16:04:17 -0500

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
   const timeString = document.getElementById('time').value
+
+  // console.log(timeString)
+
   displayMessage(greet(timeString))
 }
 
@@ -16,13 +19,23 @@ function handleClick(e) {
 // if the time is between 12pm and 5pm, return "Good Afternoon".
 // if the time is later than 5pm, return "Good Evening".
 // body input#time
-function greet() {
-
+function greet(time) {
+  let splitTime = time.split(":")
+  let parsedTime = parseInt(splitTime[0])
+  if (parsedTime < 12) {
+    return "Good Morning"
+  }
+  else if (parsedTime > 12 && parsedTime < 17) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
 }
 
 /* Write your implementation of displayMessage() */
 // accepts an argument of a string
 // when the function runs it should update the text inside #greeting node with the content of the argument
-function displayMessage() {
-
+function displayMessage(greet) {
+  document.getElementById('greeting').innerHTML = greet
 }
